30e7d45cf582c21cf082b5072fad4ab07b731364
sam_arbid <samarbid13@users.noreply.github.com>
2019-12-23 00:56:59 +0100

    add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   plugins: [
-    require("postcss-import"),
+    // ...
     require("tailwindcss"),
-    require("autoprefixer")
+    require("autoprefixer"),
+    process.env.NODE_ENV ==='production' && require('@fullhuman/postcss-purgecss')({
+      content: [
+        './src/**/*.vue',
+        './public/index.html',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+    
   ]
 };
